b82a59b52dce1918558e1d6af570d180a4b801f5
Gun-od Garig <gun-od.garig@elev.ga.ntig.se>
2024-11-28 16:18:36 +0100

    IMCOOKING

--- NäsanöverE03/Program.cs
+++ NäsanöverE03/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Hello32();
+
+// static void Hello32()
+// {
+
+//     int count = 0;
+
+// while (count < 32) 
+// {
+//     Console.WriteLine("Hello Word!");
+//     count++;
+// }
+
+// Console.ReadLine();
+// }
+
+///////////////////////////////////////////////////////////////////////////////////
+
+
+// Square();
+
+// static void Square()
+// {
+//     Console.WriteLine("Skriv in en siffra: ");
+//     string siffra = Console.ReadLine();
+//     int nummer;
+
+//     if (int.TryParse(siffra, out nummer))
+//     {
+//         int kvadrat = nummer * nummer;
+//         Console.WriteLine($"Kvadraten av {nummer} är {kvadrat}");
+//     }
+// }
+// Console.ReadLine();
+
+//////////////////////////////////////////////////////////////////////
+
+// Multi();
+
+// static void Multi()
+// {
+//     Console.WriteLine("Skriv in ditt första tal: ");
+//     string första = Console.ReadLine();
+
+//     Console.WriteLine("Skriv in ditt andra tal: ");
+//     string andra = Console.ReadLine();
+
+//     int nummer1;
+//     int nummer2;
+
+//     if (int.TryParse(första, out nummer1) && int.TryParse(andra, out nummer2))
+//     {
+//         int resultat = nummer1 * nummer2;
+
+//         Console.WriteLine($"Produkten av {nummer1} och {nummer2} är {resultat}");
+//     }
+// }
+// Console.ReadLine();
+
+////////////////////////////////////////////////////////
+
+RightTriangleArea();
+
+static void RightTriangleArea()
+{
+
+}
